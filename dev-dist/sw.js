0e0f4ac8b92f4190514da60537cc68068206101f
shinethit <shinethitsmt@gmail.com>
2026-09-26 16:06:59 +0000

    Remove incompatible bun.lock to fix Cloudflare Pages build

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-afac4cd2'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.goe36o9uf48"
+    "revision": "0.jdakgrrcp7"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
